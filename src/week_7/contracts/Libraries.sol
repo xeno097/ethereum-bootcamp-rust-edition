c9de60fa16e4b0dcb51f61805dffd4a3f53c3ebc
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-01-17 18:00:19 -0400

    feat(week-7): ported exercises from the libraries chapter

--- src/week_7/contracts/Libraries.sol
+++ src/week_7/contracts/Libraries.sol
@@ -2,23 +2,43 @@
 pragma solidity ^0.8.4;
 
 library UIntFunctions {
-    
-
-    function isEven(uint number) external pure returns(bool){
+    function isEven(uint256 number) external pure returns (bool) {
         return number % 2 == 0;
     }
 }
 
 contract Game {
-    using UIntFunctions for uint;
-    uint public participants;
+    using UIntFunctions for uint256;
+
+    uint256 public participants;
     bool public allowTeams;
 
-    constructor(uint _participants){
+    constructor(uint256 _participants) {
         participants = _participants;
         allowTeams = participants.isEven();
     }
+}
 
+library Prime {
+    function dividesEvenly(uint256 num1, uint256 num2) external pure returns (bool) {
+        return num1 % num2 == 0;
+    }
 
+    function isPrime(uint256 num) external pure returns (bool) {
+        for (uint256 i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
 
+        return true;
+    }
+}
+
+contract PrimeGame {
+    using Prime for uint256;
+
+    function isWinner() external view returns (bool) {
+        return block.number.isPrime();
+    }
 }
--- src/week_7/libraries.rs
+++ src/week_7/libraries.rs
@@ -11,6 +11,17 @@ abigen!(
         function participants() external pure returns(uint)
         function allowTeams() external pure returns(bool)
     ]"#;
+
+    Prime,
+    r#"[
+        function dividesEvenly(uint,uint) external pure returns(bool)
+        function isPrime(uint) external pure returns(bool)
+    ]"#;
+
+    PrimeGame,
+    r#"[
+        function isWinner() external view returns(bool)
+    ]"#;
 );
 
 #[cfg(test)]
@@ -60,13 +71,13 @@ mod tests {
     }
 
     mod using_library {
-        use std::{error::Error, sync::Arc};
+        use std::error::Error;
 
-        use ethers::{prelude::ContractFactory, solc::Solc, types::U256};
+        use ethers::types::U256;
 
         use crate::{
-            utils::{deploy_contract, get_provider_with_signer, ClientWithSigner},
-            week_7::libraries::{tests::CONTRACT_PATH, Game, UIntFunctions},
+            utils::{deploy_contract, deploy_contract_with_library, ClientWithSigner},
+            week_7::libraries::{tests::CONTRACT_PATH, Game},
         };
 
         const CONTRACT_NAME: &str = "Game";
@@ -74,6 +85,9 @@ mod tests {
         #[tokio::test]
         async fn should_set_correctly_the_participants_property() -> Result<(), Box<dyn Error>> {
             // Arrange
+            let library_instance =
+                deploy_contract(CONTRACT_PATH, "UIntFunctions", (), None).await?;
+
             let test_cases = vec![
                 U256::from(2),
                 U256::from(4),
@@ -83,39 +97,17 @@ mod tests {
                 U256::from(5),
             ];
 
-            let library_instance: UIntFunctions<ClientWithSigner> =
-                deploy_contract(CONTRACT_PATH, "UIntFunctions", (), None)
-                    .await?
-                    .into();
-
-            let signer = get_provider_with_signer(None, None);
-
-            // TODO: refactor this mess
-            let compiled = Solc::default().compile_source(CONTRACT_PATH)?;
-            let contract = compiled
-                .get(CONTRACT_PATH, CONTRACT_NAME)
-                .expect("could not find contract");
-
-            let mut bytecode = contract.bin.unwrap().clone();
-
-            // let bytcode =
-            let linked_bytecode = bytecode
-                .link(CONTRACT_PATH, "UIntFunctions", library_instance.address())
-                .resolve()
-                .unwrap();
-
-            println!("{:#?}", linked_bytecode);
-
-            let game_factory = ContractFactory::new(
-                contract.abi.unwrap().clone(),
-                linked_bytecode.clone(),
-                Arc::new(signer),
-            );
-            //  end of mess
-
             for value in test_cases {
-                let contract_instance: Game<ClientWithSigner> =
-                    game_factory.clone().deploy(value)?.send().await?.into();
+                let contract_instance: Game<ClientWithSigner> = deploy_contract_with_library(
+                    CONTRACT_PATH,
+                    "UIntFunctions",
+                    CONTRACT_PATH,
+                    CONTRACT_NAME,
+                    value,
+                    library_instance.address(),
+                )
+                .await?
+                .into();
 
                 // Act
                 let res = contract_instance.participants().call().await?;
@@ -127,32 +119,176 @@ mod tests {
             Ok(())
         }
 
-        // #[tokio::test]
-        // async fn should_set_correctly_the_allow_teams_property() -> Result<(), Box<dyn Error>> {
-        //     // Arrange
-        //     let test_cases = vec![
-        //         (U256::from(2), true),
-        //         (U256::from(4), true),
-        //         (U256::from(6), true),
-        //         (U256::from(1), false),
-        //         (U256::from(3), false),
-        //         (U256::from(5), false),
-        //     ];
+        #[tokio::test]
+        async fn should_set_correctly_the_allow_teams_property() -> Result<(), Box<dyn Error>> {
+            // Arrange
+            let library_instance =
+                deploy_contract(CONTRACT_PATH, "UIntFunctions", (), None).await?;
 
-        //     for (value, expected_value) in test_cases {
-        //         let contract_instance: Game<ClientWithSigner> =
-        //             deploy_contract(CONTRACT_PATH, CONTRACT_NAME, value, None)
-        //                 .await?
-        //                 .into();
+            let test_cases = vec![
+                (U256::from(2), true),
+                (U256::from(4), true),
+                (U256::from(6), true),
+                (U256::from(1), false),
+                (U256::from(3), false),
+                (U256::from(5), false),
+            ];
 
-        //         // Act
-        //         let res = contract_instance.allow_teams().call().await?;
+            for (value, expected_value) in test_cases {
+                let contract_instance: Game<ClientWithSigner> = deploy_contract_with_library(
+                    CONTRACT_PATH,
+                    "UIntFunctions",
+                    CONTRACT_PATH,
+                    CONTRACT_NAME,
+                    value,
+                    library_instance.address(),
+                )
+                .await?
+                .into();
 
-        //         // Assert
-        //         assert_eq!(res, expected_value);
-        //     }
+                // Act
+                let res = contract_instance.allow_teams().call().await?;
 
-        //     Ok(())
-        // }
+                // Assert
+                assert_eq!(res, expected_value);
+            }
+
+            Ok(())
+        }
+    }
+
+    mod evenly_divides {
+        use std::error::Error;
+
+        use ethers::types::U256;
+
+        const CONTRACT_NAME: &str = "Prime";
+
+        use crate::{
+            utils::{deploy_contract, ClientWithSigner},
+            week_7::libraries::{tests::CONTRACT_PATH, Prime},
+        };
+
+        #[tokio::test]
+        async fn should_detect_if_a_number_is_evenly_divisible_by_the_other(
+        ) -> Result<(), Box<dyn Error>> {
+            // Arrange
+            let library_instance: Prime<ClientWithSigner> =
+                deploy_contract(CONTRACT_PATH, CONTRACT_NAME, (), None)
+                    .await?
+                    .into();
+
+            let test_cases = vec![
+                (U256::from(4), U256::from(2), true),
+                (U256::from(16), U256::from(4), true),
+                (U256::from(200), U256::from(50), true),
+                (U256::from(5), U256::from(2), false),
+                (U256::from(22), U256::from(4), false),
+                (U256::from(199), U256::from(50), false),
+            ];
+
+            for (num, div, expected_value) in test_cases {
+                // Act
+                let res = library_instance.divides_evenly(num, div).call().await?;
+
+                // Assert
+                assert_eq!(res, expected_value);
+            }
+
+            Ok(())
+        }
+    }
+
+    mod is_prime {
+        use std::error::Error;
+
+        use ethers::types::U256;
+
+        const CONTRACT_NAME: &str = "Prime";
+
+        use crate::{
+            utils::{deploy_contract, ClientWithSigner},
+            week_7::libraries::{tests::CONTRACT_PATH, Prime},
+        };
+
+        #[tokio::test]
+        async fn should_detect_if_a_number_is_prime_or_not() -> Result<(), Box<dyn Error>> {
+            // Arrange
+            let library_instance: Prime<ClientWithSigner> =
+                deploy_contract(CONTRACT_PATH, CONTRACT_NAME, (), None)
+                    .await?
+                    .into();
+
+            let test_cases = vec![
+                (U256::from(5), true),
+                (U256::from(17), true),
+                (U256::from(47), true),
+                (U256::from(4), false),
+                (U256::from(18), false),
+                (U256::from(51), false),
+            ];
+
+            for (num, expected_value) in test_cases {
+                // Act
+                let res = library_instance.is_prime(num).call().await?;
+
+                // Assert
+                assert_eq!(res, expected_value);
+            }
+
+            Ok(())
+        }
+    }
+
+    mod next_prime {
+        use std::error::Error;
+
+        use ethers::{providers::Middleware, types::U256};
+
+        use crate::{
+            utils::{deploy_contract, deploy_contract_with_library, ClientWithSigner},
+            week_7::libraries::{tests::CONTRACT_PATH, Prime, PrimeGame},
+        };
+
+        const CONTRACT_NAME: &str = "PrimeGame";
+
+        #[tokio::test]
+        async fn should_correctly_determine_a_winner() -> Result<(), Box<dyn Error>> {
+            // Arrange
+            let library_instance: Prime<ClientWithSigner> =
+                deploy_contract(CONTRACT_PATH, "Prime", (), None)
+                    .await?
+                    .into();
+
+            let contract_instance: PrimeGame<ClientWithSigner> = deploy_contract_with_library(
+                CONTRACT_PATH,
+                "Prime",
+                CONTRACT_PATH,
+                CONTRACT_NAME,
+                (),
+                library_instance.address(),
+            )
+            .await?
+            .into();
+
+            let block_number = contract_instance
+                .client()
+                .get_block_number()
+                .await?
+                .as_u64();
+
+            let expected_result = library_instance
+                .is_prime(U256::from(block_number))
+                .call()
+                .await?;
+
+            // Act
+            let res = contract_instance.is_winner().call().await?;
+
+            // Assert
+            assert_eq!(res, expected_result);
+
+            Ok(())
+        }
     }
 }
